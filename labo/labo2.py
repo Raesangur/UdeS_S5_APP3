7b0d70c476588f3bde1d0a5422a8ed448443949d
raesangur <pascalemmanuel.lachance@gmail.com>
2023-02-10 12:14:37 -0500

    comments

--- labo/labo2.py
+++ labo/labo2.py
@@ -11,26 +11,31 @@ B = 4096
 m = [fc * n / fe for n in N]
 k = [2 * n / fe for n in m]
 
+# Creation de l'impulsion frequentielle du filtre
 H = [[], [], []]
 H[0] = [1 if n <= m[0] or N[0] - n - 1 <= m[0] else 0 for n in range(N[0])]
 H[1] = [1 if n <= m[1] or N[1] - n - 1 <= m[1] else 0 for n in range(N[1])]
 H[2] = [1 if n <= m[2] or N[2] - n - 1 <= m[2] else 0 for n in range(N[2])]
 
+# Reponse impulsionnelle du filtre
 h = [[], [], []]
 h[0] = np.fft.ifft(H[0])
 h[1] = np.fft.ifft(H[1])
 h[2] = np.fft.ifft(H[2])
 
+# Padding de la reponse impulsionnelle
 hz = [[], [], []]
 hz[0] = [h[0][i] if i < len(h[0]) else 0 for i in range(B)]
 hz[1] = [h[1][i] if i < len(h[1]) else 0 for i in range(B)]
 hz[2] = [h[2][i] if i < len(h[2]) else 0 for i in range(B)]
 
+# Reponse frequentielle padded
 Hz = [[], [], []]
 Hz[0] = np.fft.fft(hz[0])
 Hz[1] = np.fft.fft(hz[1])
 Hz[2] = np.fft.fft(hz[2])
 
+# Affichage 3x3
 fig, axs = plt.subplots(3, 3)
 axs[0][0].stem(range(N[0]), H[0])
 axs[1][0].stem(range(N[0]), h[0].real)
